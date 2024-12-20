6ef56986c8c58606e8569d9f2582bb6a6ef4ad6d
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-20 15:26:40 +0100

    stylings are added to app

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -27,10 +27,41 @@ const ShoppingLists = ({ db }) => {
   return (
     <View style={styles.container}>
       <FlatList
+        style={styles.listsContainer}
         data={lists}
         renderItem={({ item }) =>
-          <Text>{item.name}: {item.items.join(", ")}</Text>}
+          <View style={styles.listItem}>
+            <Text >{item.name}: {item.items.join(", ")}</Text>
+          </View>
+        }
       />
+      <View style={styles.listForm}>
+        <TextInput
+          style={styles.listName}
+          placeholder="List Name"
+          value={listName}
+          onChangeText={setListName}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder="Item #1"
+          value={item1}
+          onChangeText={setItem1}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder="Item #2"
+          value={item2}
+          onChangeText={setItem2}
+        />
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => { }}
+        >
+          <Text style={styles.addButtonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
     </View>
   )
 }
@@ -38,6 +69,51 @@ const ShoppingLists = ({ db }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  listItem: {
+    height: 70,
+    justifyContent: "center",
+    paddingHorizontal: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: "#AAA",
+    flex: 1,
+    flexGrow: 1
+  },
+  listForm: {
+    flexBasis: 275,
+    flex: 0,
+    margin: 15,
+    padding: 15,
+    backgroundColor: "#CCC"
+  },
+  listName: {
+    height: 50,
+    padding: 15,
+    fontWeight: "600",
+    marginRight: 50,
+    marginBottom: 15,
+    borderColor: "#555",
+    borderWidth: 2
+  },
+  item: {
+    height: 50,
+    padding: 15,
+    marginLeft: 50,
+    marginBottom: 15,
+    borderColor: "#555",
+    borderWidth: 2
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    backgroundColor: "#000",
+    color: "#FFF"
+  },
+  addButtonText: {
+    color: "#FFF",
+    fontWeight: "600",
+    fontSize: 20
   }
 });
 
