9c3be11f5784c39dc1b28e49672a2ff32283d2ee
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-20 15:59:57 +0100

    Platform is imported

--- components/ShoppingLists.js
+++ components/ShoppingLists.js
@@ -1,5 +1,5 @@
 import { View ,FlatList,StyleSheet,Text,TextInput, KeyboardAvoidingView,
-  TouchableOpacity,Alert} from "react-native";
+  TouchableOpacity,Alert,Platform} from "react-native";
 
 import { useState,useEffect } from "react";
 import { collection, getDocs ,addDoc} from "firebase/firestore";
